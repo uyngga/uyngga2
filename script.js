d0b18af8d753e304fc6864f03c2164b04865fd68
uyngga <153445871+uyngga@users.noreply.github.com>
2023-12-11 02:33:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function login() {
     if (username === "user" && password === "pass") {
         // Successful login, redirect to another page
         alert("Та амжилттай нэвтэрлээ!");
-        window.location.href = "file:///C:/Users/Uyangaa/Downloads/tutorial-38/38.%20Bootstrap%202020%20Starter%20Files/index.html"; // Replace "success.html" with the desired page
+        window.location.href = "https://uyngga.github.io/Uynga/index.html"; // Replace "success.html" with the desired page
     } else {
         // Display an error message
         errorMessage.textContent = "Хэрэглэгчийн нэр эсвэл нууц үг буруу байна. Дахин оролдоно уу..";
